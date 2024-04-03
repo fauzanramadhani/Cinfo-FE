c02a1e499a1c796c46fd8f1985b661ed491ca9df
fauzanramadhani <fauzanramadhani06@gmail.com>
2024-04-03 20:27:15 +0700

    feat: setup dependencies hilt injection

---
+++ app/src/main/java/com/ndc/cinfo/core/di/Application.kt
@@ -0,0 +1,7 @@
+package com.ndc.cinfo.core.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application()
--- app/src/main/java/com/ndc/cinfo/ui/navigation/SetupNavHost.kt
+++ app/src/main/java/com/ndc/cinfo/ui/navigation/SetupNavHost.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.ndc.cinfo.ui.screen.login.LoginScreen
+import com.ndc.cinfo.ui.screen.main.MainScreen
 import com.ndc.cinfo.ui.screen.register.RegisterScreen
 
 @Composable
@@ -25,6 +26,11 @@ fun SetupNavHost(
             route = NavRoute.Register.route
         ) {
             RegisterScreen(navHostController = navHostController)
+        }
+        composable(
+            route = NavRoute.Main.route
+        ) {
+            MainScreen(navHostController = navHostController)
         }
     }
 }
