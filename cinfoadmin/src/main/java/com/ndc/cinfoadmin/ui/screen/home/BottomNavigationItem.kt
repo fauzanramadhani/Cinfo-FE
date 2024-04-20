6837750a1afaa7ea55d2aefb1553f220bd670379
fauzanramadhani <fauzanramadhani06@gmail.com>
2024-04-20 10:39:28 +0700

    fix: remove unused import

--- cinfoadmin/src/main/java/com/ndc/cinfoadmin/ui/screen/home/BottomNavigationItem.kt
+++ cinfoadmin/src/main/java/com/ndc/cinfoadmin/ui/screen/home/BottomNavigationItem.kt
@@ -1,9 +1,5 @@
 package com.ndc.cinfoadmin.ui.screen.home
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-
 data class BottomNavigationItem(
     val label: String,
     val unselectedIcon: Int,
